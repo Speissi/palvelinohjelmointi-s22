6f9402098fce6270ff52208530a8365c08cea4d4
Shijun Wang <shijun@shijun.dev>
2022-09-04 17:28:48 +0300

    vscode stuff

--- Bookstore/src/main/java/palvelinohjelmointi/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -16,7 +16,7 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
-	@RequestMapping(value = { "/booklist" })
+	@RequestMapping(value = { "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
@@ -39,7 +39,7 @@ public class BookController {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
-	
+
 	@RequestMapping(value = "/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
